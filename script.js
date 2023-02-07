daf86a2251cc7b0bec29a728ef6c1e3ba5ef045c
thompsonslee <thompson.slee@hotmail.co.uk>
2023-02-08 01:31:15 +1100

    add click event listener for 4x4 divs, and add text nodes

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const fourbyfour = document.getElementById("fourbyfour")
-
+const numgen = [7,8,9,"/",4,5,6,"x",1,2,3,"-",".",0,"=","+"]
+let numgenindex = 0
 for (let i =0 ; i<4 ; i++){
     let columndivs = document.createElement("div")
     columndivs.setAttribute("class","column")
     for(let i=0 ; i<4 ; i++){
         let rowdivs = document.createElement("div")
+        let thenumber = document.createTextNode(numgen[numgenindex])
+        rowdivs.appendChild(thenumber)
+        rowdivs.addEventListener("click",
+        e => e.target.classList.add("clicked"))
+
         rowdivs.setAttribute("class", "row")
+
         columndivs.appendChild(rowdivs)
+        numgenindex ++
     }
     fourbyfour.appendChild(columndivs)
 }
+

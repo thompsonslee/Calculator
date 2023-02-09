964304f7ba995ad0a45f02ec8f9808b62f433f38
thompsonslee <thompson.slee@hotmail.co.uk>
2023-02-09 21:57:07 +1100

    add code to save sequence of buttons clicked

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const fourbyfour = document.getElementById("fourbyfour")
 const numgen = [7,8,9,"/",4,5,6,"x",1,2,3,"-",".",0,"=","+"]
-const classset = [7,8,9,"divide",4,5,6,"multi",1,2,3,"subtract","dot",0,"equals","addition"]
+const classset = [7,8,9,"divide",4,5,6,"multi",1,2,3,"subtract",".",0,"equals","addition"]
 let numgenindex = 0
 let storingNumbers =[]
 let storingArray = []
@@ -20,7 +20,7 @@ for (let i =0 ; i<4 ; i++){
         e => e.target.classList.add("clicked",classset))
 
         rowdivs.addEventListener("click", function(){
-            storingNumbers.push(datavalue)
+            sendToStorage(datavalue)
         })
 
         rowdivs.setAttribute("class", "row")
@@ -29,4 +29,30 @@ for (let i =0 ; i<4 ; i++){
         numgenindex ++
     }
     fourbyfour.appendChild(columndivs)
+}
+
+function sendToStorage(item){
+    switch (item){
+        case "addition":
+            sendToArray(item)
+            break
+        case "subtract":
+            sendToArray(item)
+            break
+        case "multi":
+            sendToArray(item)
+            break
+        case "divide":
+            sendToArray(item)
+            break
+        default:
+            storingNumbers.push(item)
+    }
+}
+function sendToArray(item){
+    let arraytostring = storingNumbers.join("")
+    let stringtofloat = parseFloat(arraytostring)
+    storingNumbers = []
+    storingArray.push(stringtofloat)
+    storingArray.push(item)
 }

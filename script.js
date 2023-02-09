4a375b0fad79849a08c93b9bc9502cd5c6f85c3c
thompsonslee <thompson.slee@hotmail.co.uk>
2023-02-09 21:08:12 +1100

    add event listener to store items clicked in an array

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const fourbyfour = document.getElementById("fourbyfour")
 const numgen = [7,8,9,"/",4,5,6,"x",1,2,3,"-",".",0,"=","+"]
+const classset = [7,8,9,"divide",4,5,6,"multi",1,2,3,"subtract","dot",0,"equals","addition"]
 let numgenindex = 0
+let storingNumbers =[]
+let storingArray = []
+
 for (let i =0 ; i<4 ; i++){
     let columndivs = document.createElement("div")
     columndivs.setAttribute("class","column")
     for(let i=0 ; i<4 ; i++){
+
         let rowdivs = document.createElement("div")
+
         let thenumber = document.createTextNode(numgen[numgenindex])
         rowdivs.appendChild(thenumber)
+        let datavalue =classset[numgenindex]
+
         rowdivs.addEventListener("click",
-        e => e.target.classList.add("clicked"))
+        e => e.target.classList.add("clicked",classset))
+
+        rowdivs.addEventListener("click", function(){
+            storingNumbers.push(datavalue)
+        })
 
         rowdivs.setAttribute("class", "row")
 
@@ -18,4 +30,3 @@ for (let i =0 ; i<4 ; i++){
     }
     fourbyfour.appendChild(columndivs)
 }
-
